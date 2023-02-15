159251ae1377908f146961d3e789d8b894bdddeb
wangf-q <wangf-q@glodon.com>
2023-02-15 08:31:45 +0800

    transaction project init

---
+++ lagou-spring/lagou-transfer-transaction/src/main/java/com/lagou/edu/service/impl/TransferServiceImpl.java
@@ -0,0 +1,27 @@
+package com.lagou.edu.service.impl;
+
+import com.lagou.edu.dao.AccountDao;
+import com.lagou.edu.pojo.Account;
+import com.lagou.edu.service.TransferService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service("transferService")
+public class TransferServiceImpl implements TransferService {
+    @Autowired
+    private AccountDao accountDao;
+
+    @Override
+    public void transfer(String fromCardNo, String toCardNo, int money) throws Exception {
+
+        final Account from = accountDao.queryAccountByCardNo(fromCardNo);
+        final Account to = accountDao.queryAccountByCardNo(toCardNo);
+
+        from.setMoney(from.getMoney() - money);
+        to.setMoney(to.getMoney() + money);
+
+        accountDao.updateAccountByCardNo(from);
+        accountDao.updateAccountByCardNo(to);
+
+    }
+}
