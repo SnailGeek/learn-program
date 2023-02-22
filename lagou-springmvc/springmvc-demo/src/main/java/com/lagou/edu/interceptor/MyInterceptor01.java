b9e9c656c7a9508d49bd5ba663fec5fd936a908d
wangf-q <wangf-q@glodon.com>
2023-02-22 21:40:39 +0800

    myinterceptor01

---
+++ lagou-springmvc/springmvc-demo/src/main/java/com/lagou/edu/interceptor/MyInterceptor01.java
@@ -0,0 +1,64 @@
+package com.lagou.edu.interceptor;
+
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 自定义拦截器
+ *
+ * @author zero
+ */
+public class MyInterceptor01 implements HandlerInterceptor {
+    /**
+     * 业务逻辑之前执行
+     * 应用场景：完成权限校验
+     *
+     * @param request  current HTTP request
+     * @param response current HTTP response
+     * @param handler  chosen handler to execute, for type and/or instance evaluation
+     * @return 是否放行， true：放行，false表示终止
+     * @throws Exception
+     */
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        System.out.println("Myinterceptor01.......... preHandle");
+        return HandlerInterceptor.super.preHandle(request, response, handler);
+    }
+
+    /**
+     * 业务逻辑执行之后
+     *
+     * @param request      current HTTP request
+     * @param response     current HTTP response
+     * @param handler      handler (or {@link HandlerMethod}) that started asynchronous
+     *                     execution, for type and/or instance examination
+     * @param modelAndView the {@code ModelAndView} that the handler returned
+     *                     (can also be {@code null})
+     * @throws Exception
+     */
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        System.out.println("Myinterceptor01.......... postHandle");
+        HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+    }
+
+    /**
+     * 页面渲染完毕执行
+     *
+     * @param request  current HTTP request
+     * @param response current HTTP response
+     * @param handler  handler (or {@link HandlerMethod}) that started asynchronous
+     *                 execution, for type and/or instance examination
+     * @param ex       exception thrown on handler execution, if any
+     * @throws Exception
+     */
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        System.out.println("Myinterceptor01.......... afterCompletion");
+        HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
+    }
+}
