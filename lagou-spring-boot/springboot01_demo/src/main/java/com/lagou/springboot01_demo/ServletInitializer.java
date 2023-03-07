1dbf5bfe9c1e516dbd92b5f7b0c8724990d60a32
wangf-q <wangf-q@glodon.com>
2023-03-07 08:28:13 +0800

    init spring boot demo

---
+++ lagou-spring-boot/springboot01_demo/src/main/java/com/lagou/springboot01_demo/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.lagou.springboot01_demo;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Springboot01DemoApplication.class);
+    }
+
+}
