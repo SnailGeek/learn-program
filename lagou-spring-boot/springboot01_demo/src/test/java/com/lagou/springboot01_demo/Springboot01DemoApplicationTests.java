d9e3293dc8f5679e4749b697ae0eb6bac1dd6718
wangf-q <wangf-q@glodon.com>
2023-03-08 06:58:15 +0800

    test properties

--- lagou-spring-boot/springboot01_demo/src/test/java/com/lagou/springboot01_demo/Springboot01DemoApplicationTests.java
+++ lagou-spring-boot/springboot01_demo/src/test/java/com/lagou/springboot01_demo/Springboot01DemoApplicationTests.java
@@ -1,13 +1,33 @@
 package com.lagou.springboot01_demo;
 
+import com.lagou.controller.HelloController;
+import com.lagou.pojo.Person;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
+@RunWith(SpringRunner.class) //测试启动器，并加载spring boot测试注解
+@SpringBootTest // 标记该类为spring boot单元测试类，并加载项目的applicationContext上下文环境
 class Springboot01DemoApplicationTests {
+
+    @Autowired
+    private HelloController helloController;
 
     @Test
     void contextLoads() {
+        final String demo = helloController.demo();
+        System.out.println(demo);
+    }
+
+    @Autowired
+    private Person person;
+
+    @Test
+    void configurationTest() {
+        System.out.println(person);
     }
 
 }
