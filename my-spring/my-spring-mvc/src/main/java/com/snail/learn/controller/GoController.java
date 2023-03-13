fbdf9c1d8f4600e31753b0f17192d05755deeda9
wangf-q <wangf-q@glodon.com>
2023-03-13 08:31:41 +0800

    environment

--- my-spring/my-spring-mvc/src/main/java/com/snail/learn/controller/GoController.java
+++ my-spring/my-spring-mvc/src/main/java/com/snail/learn/controller/GoController.java
@@ -1,12 +1,14 @@
 package com.snail.learn.controller;
 
+import org.springframework.context.EnvironmentAware;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-public class GoController {
+public class GoController implements EnvironmentAware {
 
     @RequestMapping(value = {"/"}, method = {RequestMethod.HEAD})
     public String head() {
@@ -17,5 +19,12 @@ public class GoController {
     public String index(Model model) throws Exception {
         model.addAttribute("msg", "GO GO GO!");
         return "go.jsp";
+    }
+
+    private Environment environment = null;
+
+    @Override
+    public void setEnvironment(Environment environment) {
+        this.environment = environment;
     }
 }
