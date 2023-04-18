0fa2b672e0c7c304b062bdd85aaf53c2a582e6fb
wangf-q <wangf-q@glodon.com>
2023-04-18 22:45:29 +0800

    BeanFactory 通过编码方式实现bean的注入

--- spring-uncover-secrets/uncover-chapter2/src/test/java/NewsProviderTest.java
+++ spring-uncover-secrets/uncover-chapter2/src/test/java/NewsProviderTest.java
@@ -1,5 +1,15 @@
+import com.geek.design.news.DowJonesNewsListener;
+import com.geek.design.news.DowJonesNewsPersister;
 import com.geek.design.news.FXNewsProvider;
 import org.junit.Test;
+import org.springframework.beans.MutablePropertyValues;
+import org.springframework.beans.PropertyValue;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.config.ConstructorArgumentValues;
+import org.springframework.beans.factory.support.AbstractBeanDefinition;
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.support.RootBeanDefinition;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
@@ -24,5 +34,43 @@ public class NewsProviderTest {
         // final ApplicationContext context = new FileSystemXmlApplicationContext("/src/main/resources/beans.xml");
         final FXNewsProvider djNewsProvider = (FXNewsProvider) context.getBean("djNewsProvider");
         djNewsProvider.getAndPersistNews();
+    }
+
+    @Test
+    public void testBeanFactory() {
+        final DefaultListableBeanFactory beaRegistry = new DefaultListableBeanFactory();
+        final BeanFactory beanFactory = bindViaCode(beaRegistry);
+        final FXNewsProvider provider = (FXNewsProvider) beanFactory.getBean("djNewsProvider");
+        provider.getAndPersistNews();
+    }
+
+    /**
+     * 通过编码的方式注入
+     */
+    private BeanFactory bindViaCode(BeanDefinitionRegistry registry) {
+        final AbstractBeanDefinition newsProvider = new RootBeanDefinition();
+        //调用这个构造方法，需要有FXNewsProvider需要一个无参构造方法，这种方法后面在通过构造方法注入djNewsListener和djNewsPersister会注入失败
+        // final AbstractBeanDefinition newsProvider = new RootBeanDefinition(FXNewsProvider.class, FXNewsProvider::new);
+        newsProvider.setBeanClass(FXNewsProvider.class);
+
+        final AbstractBeanDefinition newsListener = new RootBeanDefinition(DowJonesNewsListener.class, DowJonesNewsListener::new);
+        final AbstractBeanDefinition newsPersister = new RootBeanDefinition(DowJonesNewsPersister.class, DowJonesNewsPersister::new);
+        registry.registerBeanDefinition("djNewsProvider", newsProvider);
+        registry.registerBeanDefinition("djNewsListener", newsListener);
+        registry.registerBeanDefinition("djNewsPersister", newsPersister);
+
+        // 通过构造的方式注入
+        final ConstructorArgumentValues argumentValues = new ConstructorArgumentValues();
+        argumentValues.addIndexedArgumentValue(0, newsListener);
+        argumentValues.addIndexedArgumentValue(1, newsPersister);
+        newsProvider.setConstructorArgumentValues(argumentValues);
+
+        // 或者通过Setter方法注入
+        // final MutablePropertyValues propertyValues = new MutablePropertyValues();
+        // propertyValues.addPropertyValue(new PropertyValue("newsListener", newsListener));
+        // propertyValues.addPropertyValue(new PropertyValue("newsPersister", newsPersister));
+        // newsProvider.setPropertyValues(propertyValues);
+
+        return (BeanFactory) registry;
     }
 }
