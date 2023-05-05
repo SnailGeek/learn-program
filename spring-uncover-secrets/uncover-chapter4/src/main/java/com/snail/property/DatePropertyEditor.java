6442a222264351b2e2defe9d7c868b904cd27a71
wangf-q <wangf-q@glodon.com>
2023-05-06 07:34:31 +0800

    DatePropertyEditorRegistrar

--- spring-uncover-secrets/uncover-chapter4/src/main/java/com/snail/property/DatePropertyEditor.java
+++ spring-uncover-secrets/uncover-chapter4/src/main/java/com/snail/property/DatePropertyEditor.java
@@ -6,15 +6,24 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class DatePropertyEditor extends PropertyEditorSupport {
+    private String datePattern;
 
     @Override
     public void setAsText(String text) throws IllegalArgumentException {
         try {
-            final SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy/MM/dd");
+            final SimpleDateFormat simpleDateFormat = new SimpleDateFormat(getDatePattern());
             final Date date = simpleDateFormat.parse(text);
             setValue(date);
         } catch (ParseException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public String getDatePattern() {
+        return datePattern;
+    }
+
+    public void setDatePattern(String datePattern) {
+        this.datePattern = datePattern;
     }
 }
--- spring-uncover-secrets/uncover-chapter4/src/main/java/com/snail/property/DatePropertyEditorRegistrar.java
+++ spring-uncover-secrets/uncover-chapter4/src/main/java/com/snail/property/DatePropertyEditorRegistrar.java
@@ -1,2 +1,23 @@
-package com.snail.property;public class DatePropertyEditorRegistrar {
+package com.snail.property;
+
+import org.springframework.beans.PropertyEditorRegistrar;
+import org.springframework.beans.PropertyEditorRegistry;
+
+import java.beans.PropertyEditor;
+
+public class DatePropertyEditorRegistrar implements PropertyEditorRegistrar {
+    private PropertyEditor propertyEditor;
+
+    @Override
+    public void registerCustomEditors(PropertyEditorRegistry registry) {
+        registry.registerCustomEditor(java.util.Date.class, getPropertyEditor());
+    }
+
+    public PropertyEditor getPropertyEditor() {
+        return propertyEditor;
+    }
+
+    public void setPropertyEditor(PropertyEditor propertyEditor) {
+        this.propertyEditor = propertyEditor;
+    }
 }
