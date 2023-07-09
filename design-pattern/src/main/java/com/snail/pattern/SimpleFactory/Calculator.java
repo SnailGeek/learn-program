b75a0e4b62d30cd63629fce65bcfce71c9b940d6
wangf-q <wangf-q@glodon.com>
2023-07-09 09:51:25 +0800

    设计模式工程创建

---
+++ design-pattern/src/main/java/com/snail/pattern/SimpleFactory/Calculator.java
@@ -0,0 +1,13 @@
+package com.snail.pattern.SimpleFactory;
+import java.io.Console;
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        System.out.println("11111");
+        final Scanner scanner = new Scanner(System.in);
+        final String A = scanner.nextLine();
+        System.out.println("222");
+        final String B = scanner.nextLine();
+    }
+}
