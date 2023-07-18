9529b6a31c8d5cd5420a2a975b3a286bd0136d95
wangf-q <wangf-q@glodon.com>
2023-07-18 09:29:09 +0800

    schema advice

--- spring-uncover-secrets/uncover-chapter10/src/main/java/com/snail/schema/SchemaBasedAspect.java
+++ spring-uncover-secrets/uncover-chapter10/src/main/java/com/snail/schema/SchemaBasedAspect.java
@@ -1,6 +1,7 @@
 package com.snail.schema;
 
 
+import org.apache.commons.lang.exception.ExceptionUtils;
 import org.aspectj.lang.JoinPoint;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -10,5 +11,23 @@ public class SchemaBasedAspect {
 
     public void doBefore(JoinPoint jp) {
         logger.info("beafore method[" + jp.getSignature().getName() + "] execution.");
+    }
+
+    public void doAfter(JoinPoint joinPoint) {
+        logger.info("method[" + joinPoint.getSignature().getName() + "] completed successfully.");
+    }
+
+
+    public void doAfter(JoinPoint joinPoint, Object retValue) {
+        logger.info("method[" + joinPoint.getSignature().getName() + "] completed successfully.");
+        logger.info("with return value: " + retValue);
+    }
+
+    public void doAfterThrowing(RuntimeException e) {
+        logger.info(ExceptionUtils.getFullStackTrace(e));
+    }
+
+    public void doAfter() {
+        logger.warn("release system resources, etc...");
     }
 }
