61ce1d432f4980a242adb011276a4653c83d550d
wangf-q <wangf-q@glodon.com>
2023-09-26 17:17:03 +0800

    session共享问题复现项目

---
+++ lagou-session/src/main/java/com/lagou/learn/interceptor/MyWebAppConfigurer.java
@@ -0,0 +1,18 @@
+package com.lagou.learn.interceptor;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+ 
+@Configuration
+public class MyWebAppConfigurer extends WebMvcConfigurerAdapter{
+    
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new RequestInterceptor())
+                .addPathPatterns("/**")
+                .excludePathPatterns("/login/**","/error");
+        super.addInterceptors(registry);
+    }
+}
---
+++ lagou-session/src/test/java/com/lagou/learn/LagouSessionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lagou.learn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LagouSessionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
