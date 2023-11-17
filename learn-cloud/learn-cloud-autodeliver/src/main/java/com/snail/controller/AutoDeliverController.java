014af986308096d31ed79a78989bf6a02247037b
wangf-q <wangf-q@glodon.com>
2023-11-17 08:58:39 +0800

    客户端注册到eureka

--- learn-cloud/learn-cloud-autodeliver/src/main/java/com/snail/controller/AutoDeliverController.java
+++ learn-cloud/learn-cloud-autodeliver/src/main/java/com/snail/controller/AutoDeliverController.java
@@ -1,21 +1,38 @@
 package com.snail.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/autodeliver")
 public class AutoDeliverController {
     @Autowired
     private RestTemplate restTemplate;
+    @Autowired
+    private DiscoveryClient discoveryClient;
 
     @GetMapping("/checkState/{userId}")
     public Integer checkState(@PathVariable Long userId) {
         return restTemplate.getForObject("http://localhost:8081/resume/openstate/" + userId, Integer.class);
 
+    }
+
+
+    @GetMapping("/checkState/discovery/{userId}")
+    public Integer checkStateByDiscovery(@PathVariable Long userId) {
+        List<ServiceInstance> instances = discoveryClient.getInstances("learn-cloud-resume");
+        ServiceInstance serviceInstance = instances.get(0);
+        String host = serviceInstance.getHost();
+        int port = serviceInstance.getPort();
+        String url = String.format("http://%s:%s/resume/openstate/%s", host, port, userId);
+        return restTemplate.getForObject(url, Integer.class);
     }
 }
--- learn-cloud/learn-cloud-resume/src/main/java/com/snail/ResumeApplication.java
+++ learn-cloud/learn-cloud-resume/src/main/java/com/snail/ResumeApplication.java
@@ -3,9 +3,13 @@ package com.snail;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EntityScan("com.snail.pojo")
+//@EnableEurekaClient // eureka独有注解
+@EnableDiscoveryClient // 注册到中心，通用注解，可以用于（eureka和nacos等）
 public class ResumeApplication {
     public static void main(String[] args) {
         SpringApplication.run(ResumeApplication.class, args);
