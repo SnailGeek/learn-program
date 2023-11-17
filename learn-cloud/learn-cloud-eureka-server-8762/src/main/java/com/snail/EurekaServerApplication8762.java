054a4d3f8de1a0f52bc1db71d023db6c55011b02
wangf-q <wangf-q@glodon.com>
2023-11-17 08:23:50 +0800

    eureka cluster

---
+++ learn-cloud/learn-cloud-eureka-server-8762/src/main/java/com/snail/EurekaServerApplication8762.java
@@ -0,0 +1,13 @@
+package com.snail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication8762 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication8762.class, args);
+    }
+}
