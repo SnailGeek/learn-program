4f9edd901fb377d99e6f2d21858e99f36cc787f2
wangf-q <wangf-q@glodon.com>
2023-11-30 19:00:00 +0800

    异步登录

--- learn-security/lagou-security-management/src/main/java/com/snail/service/impl/MyAuthProcessService.java
+++ learn-security/lagou-security-management/src/main/java/com/snail/service/impl/MyAuthProcessService.java
@@ -1,5 +1,8 @@
 package com.snail.service.impl;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.DefaultRedirectStrategy;
@@ -12,22 +15,37 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class MyAuthProcessService implements AuthenticationSuccessHandler,
         AuthenticationFailureHandler {
 
     private RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
         System.out.println("登录成功处理。。。");
-        redirectStrategy.sendRedirect(request, response, "/");
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("code", HttpStatus.OK.value());
+        map.put("message", "登录成功");
+        response.setContentType("application/json;charset=utf-8");
+        response.getWriter().write(objectMapper.writeValueAsString(map));
+//        redirectStrategy.sendRedirect(request, response, "/");
     }
 
     @Override
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
         System.out.println("登录失败处理。。。");
-        redirectStrategy.sendRedirect(request, response, "/toLoginPage");
+//        redirectStrategy.sendRedirect(request, response, "/toLoginPage");
+        Map<String, Object> map = new HashMap<>();
+        map.put("code", HttpStatus.UNAUTHORIZED.value());
+        map.put("message", "登录失败");
+        response.setContentType("application/json;charset=utf-8");
+        response.getWriter().write(objectMapper.writeValueAsString(map));
     }
 }
