8adb65303968aad0585e1ed594db3c1778e79d3e
wangf-q <wangf-q@glodon.com>
2024-12-14 14:08:05 +0800

    wait and notify

--- learn-concurrent/src/test/java/CompletableFutureTest.java
+++ learn-concurrent/src/test/java/CompletableFutureTest.java
@@ -1,2 +1,80 @@
-package PACKAGE_NAME;public class CompletableFuture {
+import org.junit.jupiter.api.Test;
+
+import java.util.Random;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
+
+
+public class CompletableFutureTest {
+    @Test
+    public void testCompletableFuture() {
+        CompletableFuture<Void> f1 = CompletableFuture.runAsync(() -> {
+            System.out.println("T1: 洗水壶");
+            sleep(1, TimeUnit.SECONDS);
+            System.out.println("T1: 烧开水");
+            sleep(2, TimeUnit.SECONDS);
+        });
+
+        CompletableFuture<String> f2 = CompletableFuture.supplyAsync(() -> {
+            System.out.println("T2: 洗茶壶");
+            sleep(1, TimeUnit.SECONDS);
+
+            System.out.println("T2: 洗茶杯");
+            sleep(2, TimeUnit.SECONDS);
+
+            System.out.println("T2: 拿茶叶");
+            sleep(1, TimeUnit.SECONDS);
+            return "龙井";
+        });
+
+        CompletableFuture<String> f3 = f1.thenCombine(f2, (__, tf) -> {
+            System.out.println("T1: 拿到茶叶:" + tf);
+            System.out.println("T1: 泡茶");
+            return "上茶:" + tf;
+        });
+        System.out.println(f3.join());
+    }
+
+    @Test
+    void testAND() {
+        CompletableFuture<String> f0 = CompletableFuture.supplyAsync(() -> "hello world")
+                .thenApply(s -> s + " QQ")
+                .thenApply(String::toUpperCase);
+
+        System.out.println(f0.join());
+    }
+
+    @Test
+    void testOr() {
+        CompletableFuture<String> f1 = CompletableFuture.supplyAsync(() -> {
+            int t = getRandom(5, 10);
+            sleep(5, TimeUnit.SECONDS);
+            System.out.println("f1: " + t);
+            return String.valueOf("f1: " + t);
+        });
+
+        CompletableFuture<String> f2 = CompletableFuture.supplyAsync(() -> {
+            int t = getRandom(5, 10);
+            sleep(t, TimeUnit.SECONDS);
+            System.out.println("f2: " + t);
+            return String.valueOf("f2: " + t);
+        });
+
+        CompletableFuture<String> f3 = f1.applyToEither(f2, s -> s);
+
+        System.out.println(f3.join());
+    }
+
+    void sleep(int t, TimeUnit u) {
+        try {
+            u.sleep(t);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    int getRandom(int min, int max) {
+        Random random = new Random();
+        return random.nextInt(max);
+    }
 }
--- learn-concurrent/src/test/java/StampedLockTest.java
+++ learn-concurrent/src/test/java/StampedLockTest.java
@@ -1,2 +1,35 @@
-package PACKAGE_NAME;public class StampedLockTest {
+import java.util.concurrent.locks.StampedLock;
+
+public class StampedLockTest {
+
+    static class Point {
+        private double x, y;
+        private final StampedLock sl = new StampedLock();
+
+        void move(double deltaX, double deltaY) {
+            long stamp = sl.writeLock();
+            try {
+                x += deltaX;
+                y += deltaY;
+            } finally {
+                sl.unlockWrite(stamp);
+            }
+        }
+
+        double distanceFromOrigin() {
+            long stamp = sl.tryOptimisticRead();
+            double currentX = x, currentY = y;
+            // 读期间有其他线程修改数据
+            if (!sl.validate(stamp)) {
+                stamp = sl.readLock();
+                try {
+                    currentX = x;
+                    currentY = y;
+                } finally {
+                    sl.unlockRead(stamp);
+                }
+            }
+            return Math.sqrt(currentX * currentX + currentY * currentY);
+        }
+    }
 }
