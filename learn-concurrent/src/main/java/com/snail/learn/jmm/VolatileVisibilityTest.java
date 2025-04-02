3c44c022d49ad92447551e98fd77bfb107bd7a44
wangf-q <wangf-q@glodon.com>
2025-04-03 07:55:51 +0800

    volatile

--- learn-concurrent/src/main/java/com/snail/learn/jmm/VolatileVisibilityTest.java
+++ learn-concurrent/src/main/java/com/snail/learn/jmm/VolatileVisibilityTest.java
@@ -2,5 +2,23 @@ package com.snail.learn.jmm;
 
 
 public class VolatileVisibilityTest {
-    private static volatile boolean stop = false;
+    private static volatile boolean initFlag = false;
+
+    public static void main(String[] args) throws InterruptedException {
+        new Thread(() -> {
+            System.out.println("waiting data...");
+            while (!initFlag) {
+
+            }
+            System.out.println("===============success");
+        }).start();
+        Thread.sleep(2000);
+        new Thread(() -> prepareData()).start();
+    }
+
+    private static void prepareData() {
+        System.out.println("prepare data...");
+        initFlag = true;
+        System.out.println("prepare data end...");
+    }
 }
